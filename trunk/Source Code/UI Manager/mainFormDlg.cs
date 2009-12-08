09761635a1d10c33afa733b601b12314758380df
anugrah.atreya <anugrah.atreya@f5c5000b-b533-0410-88bc-879f150363ec>
2009-12-08 13:50:37 +0000

    Unit testing fixes: 1. renamed button1 to btnPaidBy 2. checked if DataTable exists in dataSet before using it. 3. checked if items exists in list before using it.

--- trunk/Source Code/UI Manager/mainFormDlg.cs
+++ trunk/Source Code/UI Manager/mainFormDlg.cs
@@ -27,7 +27,7 @@ namespace ExpenseManager
 		private System.Windows.Forms.Label lblDescrition;
 		
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button btnPaidBy;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox txtPaidBy;
 		private System.Windows.Forms.GroupBox groupBox1;
@@ -105,7 +105,7 @@ namespace ExpenseManager
             this.txtPaidBy = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lstMembers = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPaidBy = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtDescription = new System.Windows.Forms.TextBox();
             this.lblDescrition = new System.Windows.Forms.Label();
@@ -160,7 +160,7 @@ namespace ExpenseManager
             this.tabPayment.Controls.Add(this.txtPaidBy);
             this.tabPayment.Controls.Add(this.label1);
             this.tabPayment.Controls.Add(this.lstMembers);
-            this.tabPayment.Controls.Add(this.button1);
+            this.tabPayment.Controls.Add(this.btnPaidBy);
             this.tabPayment.Controls.Add(this.label2);
             this.tabPayment.Controls.Add(this.txtDescription);
             this.tabPayment.Controls.Add(this.lblDescrition);
@@ -216,14 +216,14 @@ namespace ExpenseManager
             this.lstMembers.Size = new System.Drawing.Size(112, 134);
             this.lstMembers.TabIndex = 10;
             // 
-            // button1
+            // btnPaidBy
             // 
-            this.button1.Location = new System.Drawing.Point(248, 112);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(40, 24);
-            this.button1.TabIndex = 9;
-            this.button1.Text = ">>";
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnPaidBy.Location = new System.Drawing.Point(248, 112);
+            this.btnPaidBy.Name = "btnPaidBy";
+            this.btnPaidBy.Size = new System.Drawing.Size(40, 24);
+            this.btnPaidBy.TabIndex = 9;
+            this.btnPaidBy.Text = ">>";
+            this.btnPaidBy.Click += new System.EventHandler(this.btnPaidBy_Click);
             // 
             // label2
             // 
